5656364d1b85cd0c58c1dc514c62272325fae6f5
Brian Crider <brian.crider@gmail.com>
2013-01-25 00:05:32 -0600

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by Brian Crider on 1/25/13.
+//  Copyright (c) 2013 Brian Crider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
